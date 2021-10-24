47ac1879adeff2b9522f222e3954b1d0715ebc9c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-10-24 18:36:17 +0800

    extract cli.ts into main.ts for testing

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { compare } from '@beenotung/tslib/compare';
+import { format_byte } from '@beenotung/tslib/format';
+import { scan } from './core';
+
+export function main(root = '.') {
+  scan(root)
+    .then(files => {
+      files.sort((a, b) => -compare(a.size, b.size));
+      files.forEach(file =>
+        console.log(file.size, format_byte(file.size), file.pathname),
+      );
+      // console.log(files)
+    })
+    .catch(e => {
+      console.error(e);
+      process.exit(1);
+    });
+}
